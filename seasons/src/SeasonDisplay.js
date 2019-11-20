0f2889e4b46fe83dda582ecec1c59ce886c376ce
Isahann Hanacleto <isahanacleto@gmail.com>
2019-11-20 16:38:39 -0300

    finalizing project features

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,41 @@
+import './SeasonDisplay.css';
+
 import React from 'react';
 
-const SeasonDisplay = () => {
-  return <div>x</div>;
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr, it is chilly',
+    iconName: 'snowflake'
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
+const SeasonDisplay = props => {
+  const currentSeason = getSeason(props.lat, new Date().getMonth());
+
+  console.log(`User's latitude: ${props.lat}`);
+  console.log(`Current season: ${currentSeason}`);
+
+  const { text, iconName } = seasonConfig[currentSeason];
+
+  return (
+    <div className={`season-display ${currentSeason}`}>
+      <i className={`icon-left massive ${iconName} icon`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${iconName} icon`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,43 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // Avoid doing data loading at the constructor, best if done inside componentDidMount()
+  // Good place to do one-time setup
+  //   constructor(props) {
+  //     super(props);
 
-    // The only situation we direct assign a value to this.state
-    this.state = { lat: null, errorMessage: '' };
+  //     // The only situation we direct assign a value to this.state
+  //     this.state = { lat: null, errorMessage: '' };
+  //   }
+
+  // Equivalent to the previous constructor call
+  state = { lat: null, errorMessage: '' };
+
+  // Best place to do data-loading
+  componentDidMount() {
+    console.log('App component rendered to the screen with componentDidMount()');
 
     // First argument: what happens if executed successfully
     // Second argument: what happens if it gives a error
     window.navigator.geolocation.getCurrentPosition(
-      position => {
+      position =>
         // ALWAYS use setState() to update state
-        // After the setState() call, it wil rerender the component
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => {
+        // After the setState() call, it wil rerender the component by the render() method
+        this.setState({ lat: position.coords.latitude }),
+      err =>
         // console.log(err);
-        this.setState({ errorMessage: err.message });
-      }
+        this.setState({ errorMessage: err.message })
     );
   }
 
+  // Good place to do data-loading when state/props change
+  componentDidUpdate() {
+    console.log('App component was just updated componentDidUpdate()');
+  }
+
+  // Good place to do cleanup (non-react stuff)
+  //   componentWillUnmount() {}
+
   // Required method for class based components
+  // Avoid doing anything besides returning JSX
   render() {
-    console.log('component rendered!');
-    // return (
-    //   <div>
-    //     Latitude: {this.state.lat}
-    //     <br />
-    //     Error: {this.state.errorMessage}
-    //   </div>
-    // );
+    console.log('App component rendered by render()');
+
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
+      // setState() will also rerender child components if needed
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading your location, please wait...</div>;
