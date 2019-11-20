eed853c75a31e32306d36cca10948e87a7efa6be
Isahann Hanacleto <isahanacleto@gmail.com>
2019-11-20 15:05:22 -0300

    Starting to use state

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SeasonDisplay = () => {
+  return <div>x</div>;
+};
+
+export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    // The only situation we direct assign a value to this.state
+    this.state = { lat: null };
+
+    // First argument: what happens if executed successfully
+    // Second argument: what happens if it gives a error
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        // ALWAYS use setState() to update state
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
+  }
+
+  // Required method for class based components
+  render() {
+    // console.log('component rendered!');
+    return <div>Latitude: {this.state.lat}</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
