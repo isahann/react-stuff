a0c3973ebfddce6ceb1c527608298378ce971a47
Isahann Hanacleto <isahanacleto@gmail.com>
2019-11-20 15:26:20 -0300

    checking different scenarios with state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -6,23 +6,41 @@ class App extends React.Component {
     super(props);
 
     // The only situation we direct assign a value to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
 
     // First argument: what happens if executed successfully
     // Second argument: what happens if it gives a error
     window.navigator.geolocation.getCurrentPosition(
       position => {
         // ALWAYS use setState() to update state
+        // After the setState() call, it wil rerender the component
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        // console.log(err);
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // Required method for class based components
   render() {
-    // console.log('component rendered!');
-    return <div>Latitude: {this.state.lat}</div>;
+    console.log('component rendered!');
+    // return (
+    //   <div>
+    //     Latitude: {this.state.lat}
+    //     <br />
+    //     Error: {this.state.errorMessage}
+    //   </div>
+    // );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading your location, please wait...</div>;
   }
 }
 
