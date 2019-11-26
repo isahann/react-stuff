d7a87e8080e8dac65e0a0e8a5fb8c0801ce40e28
Isahann Hanacleto <isahanacleto@gmail.com>
2019-11-26 10:29:54 -0300

    listing videos

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import youtube from '../apis/youtube';
+
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    console.log(term);
+
+    const response = await youtube.get('/search', {
+      params: {
+        q: term
+      }
+    });
+
+    console.log(response);
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
-    return <div>App component</div>;
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have{' '}
+        {this.state.videos.length} videos.
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
   }
 }
 
---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    // TODO: make sure we call callback from parent component
+    this.props.onFormSubmit(this.state.term);
+  };
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label htmlFor="search">Video search</label>
+            <input
+              name="search"
+              placeholder="Search for some videos..."
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
