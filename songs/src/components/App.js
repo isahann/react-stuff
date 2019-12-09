bc7ba85b70a1026aa3ebc2462bdc54718f942154
Isahann Hanacleto <isahanacleto@gmail.com>
2019-12-09 10:51:02 -0300

    Creating songlist

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
+import SongList from './SongList';
 // When the file is not specified, webpack automatically imports index.js
 // If its an named export, you need the curly braces
 // import { selectSong } from '../actions';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
-const SongList = () => {
-  return <div>SongList</div>;
+class SongList extends React.Component {
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="content">{song.title}</div>
+
+          <div className="right floated content">
+            <button
+              onClick={() => this.props.selectSong(song)}
+              className="ui button primary"
+            >
+              Select
+            </button>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui divided list">{this.renderList()}</div>;
+  }
+}
+
+// Sets up the state of redux store inside the props of this component
+const mapStateToProps = state => {
+  console.log(state);
+  return { songs: state.songs };
 };
 
-export default SongList;
+// The connect() function connects this component with the redux store provider,
+// at the very top of our application
+// The selectSong action will be passed as props tho the component aswell,
+// and as an argument to the connect() function so that redux uses it to the dispatch function
+export default connect(mapStateToProps, { selectSong })(SongList);
