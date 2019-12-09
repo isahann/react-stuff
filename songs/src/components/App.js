e1fa21919b78c4700dde7c1f2e4e3c0adc051ab3
Isahann Hanacleto <isahanacleto@gmail.com>
2019-12-09 09:51:10 -0300

    Setting up basics of songs project

---
+++ songs/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// When the file is not specified, webpack automatically imports index.js
+// If its an named export, you need the curly braces
+// import { selectSong } from '../actions';
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
---
+++ songs/src/components/SongDetail.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SongDetail = () => {
+  return <div>SongDetail</div>;
+};
+
+export default SongDetail;
